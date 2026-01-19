a2f31047ce64285a5520ee3d695dd57007c9be11
davidhilscher <davidhilscher13@gmail.com>
2026-01-19 14:01:42 -0500

    Add LC 49 to strings folder

---
+++ strings/leetcode49.py
@@ -0,0 +1,19 @@
+""" LC 49 - strings """
+
+
+from collections import defaultdict # dict that defaults to __blank__ (list, 0, etc.)
+
+def groupAnagrams(strs):
+    anagrams = defaultdict(list) # dict that defaults to list 
+        
+    for s in strs:
+        key = ''.join(sorted(s)) # sorts strs, then joins, if anagram, it's equal
+        anagrams[key].append(s) # appends str to key in dict "anagrams"
+        
+    return list(anagrams.values()) # returns values of each key in "anagrams"
+
+
+
+print(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
+
+# the above test case should return: [['eat', 'tea', 'ate'], ['tan', 'nat'], ['bat']]
