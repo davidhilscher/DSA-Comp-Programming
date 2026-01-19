577fc3fb5c378995f54edec3e47d245d225a8c5c
davidhilscher <davidhilscher13@gmail.com>
2026-01-18 22:10:14 -0500

    Add two new LC solutions

---
+++ two_pointer/leetcode121.py
@@ -0,0 +1,18 @@
+""" LC 121 - Two Pointer"""
+
+
+def maxProfit(prices):
+    l, r = 0, 1
+    maxP = 0
+
+    while r < len(prices):
+        if prices[l] < prices[r]:
+            profit = prices[r] - prices[l]
+            maxP = max(profit, maxP)
+        else:
+            l = r
+        r += 1
+
+    return maxP
+
+print(maxProfit([7,1,5,3,6,4])) # returns 5, (6-1). Cannot sell before you buy
